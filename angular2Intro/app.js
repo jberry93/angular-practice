d354d4a1db33445a16fad82563507a3b54c7bf29
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-11-30 15:01:04 -0800

    Wrapped the component and event listener in an iife

--- angular2Intro/app.js
+++ angular2Intro/app.js
@@ -1,12 +1,16 @@
 "use strict";
 
-var AppComponent = ng.Component({
-  selector: "my-app",
-  template: "<h1>An Angular 2 App</h1>"
-}).Class({
-  constructor: function() { }
-});
+(function() {
+  
+  var AppComponent = ng.Component({
+    selector: "my-app",
+    template: "<h1>An Angular 2 App</h1>"
+  }).Class({
+    constructor: function() { }
+  });
 
-document.addEventListener("DOMContentLoaded", function() {
-  ng.bootstrap(AppComponent);
-});
+  document.addEventListener("DOMContentLoaded", function() {
+    ng.bootstrap(AppComponent);
+  });
+
+})();
