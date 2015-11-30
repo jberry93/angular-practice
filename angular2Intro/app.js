dfcda66df9e3216c3ac04d0bfe17aa35d8e353de
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-11-30 15:59:34 -0800

    Changed template to reflect changes for 'live-server'

--- angular2Intro/app.js
+++ angular2Intro/app.js
@@ -1,10 +1,10 @@
 "use strict";
 
 (function() {
-  
+
   var AppComponent = ng.Component({
     selector: "my-app",
-    template: "<h1>An Angular 2 App</h1>"
+    template: "<h1>An Angular 2 App</h1><h5>Angular 2 is Awesome!</h5>"
   }).Class({
     constructor: function() { }
   });
