e4925fcd07356d2659ce5ab0c79a680475c9638c
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-12-03 16:45:24 -0800

    Successfully displayed data from an array

--- displayData/app.js
+++ displayData/app.js
@@ -3,10 +3,20 @@
 (function() {
   var displayComponent = ng.Component({
     selector: "display",
-    template: "<p>My name is {{ name }}</p>" +
+    template:
+    "<p>My name is {{ myName }}</p>" +
+    "<p>List of Friends:</p>" +
+    "<ul>" +
+      "<li *ng-for='#name of listOfNames'>" +
+        "{{ name }}" +
+      "</li>" +
+    "</ul>" +
+    "<p *ng-if='listOfNames.length > 3'>You have many friends!</p>" +
+    "<p *ng-if='listOfNames.length <= 3'>Go make more friends!</p>",
   }).Class({
     constructor: function() {
-      this.name = "Jeff";
+      this.myName = "Jeff";
+      this.listOfNames = ["Tom", "Jake", "Brittany", "Tim", "Jane", "Jen"];
     }
   });
   document.addEventListener("DOMContentLoaded", function() {
