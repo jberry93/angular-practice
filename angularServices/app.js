b4dd96426de4b5f6d410388c67cd0484f1564401
Jeffrey Berry <jeffreyberry93@gmail.com>
2016-01-23 16:28:06 -0800

    Completed the simple function within the factory

--- angularServices/app.js
+++ angularServices/app.js
@@ -6,4 +6,21 @@ var angularApp = angular.module("app", []);
 
 var angularModule = angularApp.module("app");
 
-var angularFactory = angularModule.factory("messages", function() {...});
+var angularFactory = angularModule.factory("messages", function() {
+  // store service dependencies
+  var messages = {};
+  
+  // store messages
+  messages.list = [];
+
+  // add messages to list
+  messages.add = function(message) {
+    messages.list.push({
+      id: messages.list.length,
+      text: message
+    });
+  };
+
+  // send object containing messages
+  return messages;
+});
